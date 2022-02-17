dff85d0723d8edf11eee9dec381adc99729ea0a8
Medgyesi Zsombor <medgyesizs123@gmail.com>
2022-02-17 17:12:19 +0100

    finished with the task, created a html file to see the output

--- index.js
+++ index.js
@@ -1,69 +1,169 @@
-let mushrooms = ["L 10", "C 5", "L 6"];
+let numbersOfMushrooms = 6;
+let capacity = 25;
+let mushrooms = [
+  ["C", 4],
+  ["R", 8],
+  ["L", 12],
+  ["C", 10],
+  ["L", 2],
+  ["R", 2],
+];
 
-const hasCsiperke = (mushrooms) => {
+/**
+ * check if mushroom has csiperke, true if yes
+ * @param mushrooms
+ */
+const includeCsiperke = (mushrooms) => {
+  return mushrooms.some((m) => m[1].includes("C"));
+};
+
+/**
+ * find the largest mushroom
+ * @param mushrooms
+ */
+const largestMushroom = (mushrooms) => {
+  let sortedMushrooms = mushrooms.sort((a, b) => b[2] - a[2]);
+  return sortedMushrooms[0];
+};
+
+/**
+ * find the largest mushroom but only csiperke, and roka
+ * @param mushrooms
+ */
+const largestMushroomWithoutL = (mushrooms) => {
+  const sortedMushrooms = mushrooms.sort((a, b) => b[2] - a[2]);
+  const filteredL = sortedMushrooms.filter((m) => m[1] !== "L");
+  return filteredL[0];
+};
+
+/**
+ * get all the mushroom, which has the same weight as the current
+ * @param mushrooms
+ * @param currentMushroom
+ */
+const getAllTheSameWeight = (mushrooms, currentMushroom) => {
+  return mushrooms.filter((m) => m[2] === currentMushroom[2]);
+};
+
+/**
+ * give index to mushrooms to make them unique
+ * @param mushrooms
+ */
+const giveIndexToTheMushrooms = (mushrooms) => {
   for (let i = 0; i < mushrooms.length; i++) {
-    if (mushrooms[i].includes("C")) {
-      break;
-    }
+    mushrooms[i].unshift(i);
   }
 };
 
-const maxWeight = (a, b) => {
-  if (a > b) {
-    return a;
-  } else {
-    return b;
-  }
-};
-
-const pickingMushroom = (mushroomNumber, capacity, mushrooms) => {
-  let newArray = [];
-  let maxArray = [];
+/**
+ * sort the mushrooms by type and put them in a new array
+ * @param mushrooms
+ */
+const orderedByValue = (mushrooms) => {
+  let csiperke = [];
+  let roka = [];
+  let lila = [];
+  let result = [];
 
   for (let i = 0; i < mushrooms.length; i++) {
-    newArray.push(mushrooms[i].split(" "));
-  }
-
-  if (!hasCsiperke) {
-    let max = 0;
-    for (let i = 0; i < mushrooms.length - 1; i++) {
-      if (max < maxWeight(Number(newArray[i][1]), Number(newArray[i + 1][1]))) {
-        max = maxWeight(Number(newArray[i][1]), Number(newArray[i + 1][1]));
-      }
+    if (mushrooms[i][1].includes("C")) {
+      csiperke.push(mushrooms[i]);
+    } else if (mushrooms[i][1].includes("R")) {
+      roka.push(mushrooms[i]);
+    } else if (mushrooms[i][1].includes("L")) {
+      lila.push(mushrooms[i]);
     }
-    console.log(max);
   }
 
-  if (hasCsiperke) {
-    let max = 0;
-    for (let i = 0; i < mushrooms.length - 1; i++) {
-      if (max < maxWeight(Number(newArray[i][1]), Number(newArray[i + 1][1]))) {
-        if (Number(newArray[i][1]) > Number(newArray[i + 1][1]) && newArray[i][0].includes("L")) {
-                if(Number(newArray[i + 1][1]) > max && !newArray[i + 1][0].includes("L") ) {
-                max = newArray[i + 1][1];
-                } else {
-                    max = max;
-                }
-        } else if (Number(newArray[i][1]) < Number(newArray[i + 1][1]) && newArray[i + 1][0].includes("L")) {
-                if (Number(newArray[i][1]) > max && !newArray[i][0].includes("L")) {
-                    max = newArray[i][1];
-                } else {
-                    max = max;
-                }
+  result = [...csiperke, ...roka, ...lila];
+  return result;
+};
+
+/**
+ * counts the weight of mushrooms by type
+ * @param mushrooms
+ * @param type
+ */
+const createResult = (mushrooms, type) => {
+  const mushs = mushrooms.filter((m) => m[1] === type);
+  let sum = 0;
+  for (let i = 0; i < mushs.length; i++) {
+    sum += mushs[i][2];
+  }
+  return { count: mushs.length, sum };
+};
+
+/**
+ * run all the logic of the task
+ * @param numbersOfMushrooms
+ * @param capacity
+ * @param mushrooms
+ */
+const solve = (numbersOfMushrooms, capacity, mushrooms) => {
+  if (numbersOfMushrooms !== mushrooms.length) {
+    return;
+  }
+
+  let result = [];
+  let max = 0;
+
+  while (mushrooms.length > 0) {
+    let largestM = largestMushroom(mushrooms);
+    if (largestM[2] + max > capacity) {
+      mushrooms = mushrooms.filter((m) => m[0] !== largestM[0]);
+    } else {
+      if (largestM[1].includes("L") && includeCsiperke(mushrooms)) {
+        largestM = largestMushroomWithoutL(mushrooms);
+        let sameMushrooms = getAllTheSameWeight(mushrooms, largestM);
+        if (sameMushrooms.length >= 2) {
+          largestM = orderedByValue(sameMushrooms)[0];
+          result.push(largestM);
+          max = max + largestM[2];
+          mushrooms = mushrooms.filter((m) => m[0] !== largestM[0]);
         } else {
-          max = maxWeight(Number(newArray[i][1]), Number(newArray[i + 1][1]));
+          result.push(largestM);
+          max = max + largestM[2];
+          mushrooms = mushrooms.filter((m) => m[0] !== largestM[0]);
+        }
+      } else {
+        let sameMushrooms = getAllTheSameWeight(mushrooms, largestM);
+        if (sameMushrooms.length >= 2) {
+          largestM = orderedByValue(sameMushrooms)[0];
+          result.push(largestM);
+          max = max + largestM[2];
+          mushrooms = mushrooms.filter((m) => m[0] !== largestM[0]);
+        } else {
+          result.push(largestM);
+          max = max + largestM[2];
+          mushrooms = mushrooms.filter((m) => m[0] !== largestM[0]);
         }
       }
-      console.log(max + 'ez most a max')
     }
-    console.log(max + " ez a max");
   }
 
-  for (let i = 0; i < newArray.length; i++) {
-    maxArray.push(newArray[i].join(" "));
-  }
-
+  return { max, result };
 };
 
-//pickingMushroom(3, 100, ["L 10", "C 5", "L 6"]);
-pickingMushroom(6, 25, ['C 4', 'L 12',  'L 2', 'R 2'])
+console.time("Execution time");
+giveIndexToTheMushrooms(mushrooms);
+let res = solve(numbersOfMushrooms, capacity, mushrooms);
+
+console.log(res.result.length + " " + res.max);
+
+let csiperke = createResult(res.result, "C");
+console.log(csiperke.count + " " + csiperke.sum);
+
+let roka = createResult(res.result, "R");
+console.log(roka.count + " " + roka.sum);
+
+let lila = createResult(res.result, "L");
+console.log(lila.count + " " + lila.sum);
+
+document.getElementById("sum").textContent = res.result.length + " " + res.max;
+document.getElementById("csiperke").textContent =
+  csiperke.count + " " + csiperke.sum;
+document.getElementById("rokagomba").textContent = roka.count + " " + roka.sum;
+document.getElementById("lilapereszke").textContent =
+  lila.count + " " + lila.sum;
+
+console.timeEnd("Execution time");
